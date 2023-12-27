5ced2dd7b4f5fde8b1dcbfbbda7c2b0b5b44a8c7
James Kao <jmk17@cornell.edu>
2023-12-27 12:58:50 +0900

    Updated to 2023

--- app.js
+++ app.js
@@ -3,21 +3,43 @@ const obs = new OBSWebSocket()
 
 const CronJob = require('cron').CronJob;
 
-function retract() {
-  obs.connect({address: '127.0.0.1:4444'})
-    .then(
-      script => {
-        obs.send('TriggerHotkeyByName', { hotkeyName: 'ROTATE_ccw' })
-          .catch( err => console.log(err))
-          .finally( () => obs.disconnect() )
-      },
-      error => console.log(error)
-    );
+async function wsOneShot(command, data) {
+  try {
+    await obs.connect({address: '127.0.0.1:4444'})
+    await obs.send(command, data)
+  } catch (err) {
+    console.log(err)
+    obs.disconnect()
+    try {
+      await obs.connect({address: '127.0.0.1:4444'})
+      await obs.send(command, data)  
+    } catch (err) {
+      // Do nothing
+      console.log("Retry failed, skipping...")
+    } finally {
+      obs.disconnect()
+    }
+  } finally {
+    obs.disconnect()
+  }
+}
 
+function retract() {
+  wsOneShot('TriggerHotkeyBySequence', { keyId: 'OBS_KEY_NUMASTERISK' })
   console.log('OBS Hotkey Retract at: ' + new Date().toLocaleString())
 }
 
-const job0 = new CronJob('30 */2 * * * ', retract)
-const job1 = new CronJob('10,50 1-23/2 * * *', retract)
+function screenshot() {
+  wsOneShot('TriggerHotkeyByName', { hotkeyName: 'OBSBasic.Screenshot' })
+  console.log('OBS Screenshot at: ' + new Date().toLocaleString())
+}
+
+const job0 = new CronJob('10,50 */2 * * * ', retract)
+const job1 = new CronJob('30 1-23/2 * * *', retract)
+// const job2 = new CronJob('*/15 * * * *', screenshot)
+
 job0.start()
 job1.start()
+// job2.start()
+
+console.log('Timetable Cron Launched')
