b8d9b76d68cf1e7f954f87898ddfefc5f42fc829
James Kao <jmk17@cornell.edu>
2023-12-27 14:12:06 +0900

    Updated for 2024 with time offset switch at PST midnight.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const OBSWebSocket = require('obs-websocket-js')
+const OBSWebSocket = require('obs-websocket-js').default
 const obs = new OBSWebSocket()
 
 const CronJob = require('cron').CronJob;
 
 async function wsOneShot(command, data) {
   try {
-    await obs.connect({address: '127.0.0.1:4444'})
-    await obs.send(command, data)
+    // await obs.connect({address: '127.0.0.1:4444'})
+    // await obs.send(command, data)
+    await obs.connect()
+    await obs.call(command, data)
   } catch (err) {
     console.log(err)
     obs.disconnect()
     try {
-      await obs.connect({address: '127.0.0.1:4444'})
-      await obs.send(command, data)  
+      await obs.connect()
+      await obs.call(command, data)
     } catch (err) {
       // Do nothing
       console.log("Retry failed, skipping...")
@@ -25,7 +27,7 @@ async function wsOneShot(command, data) {
 }
 
 function retract() {
-  wsOneShot('TriggerHotkeyBySequence', { keyId: 'OBS_KEY_NUMASTERISK' })
+  wsOneShot('TriggerHotkeyByKeySequence', { keyId: 'OBS_KEY_NUMASTERISK' })
   console.log('OBS Hotkey Retract at: ' + new Date().toLocaleString())
 }
 
@@ -34,12 +36,13 @@ function screenshot() {
   console.log('OBS Screenshot at: ' + new Date().toLocaleString())
 }
 
-const job0 = new CronJob('10,50 */2 * * * ', retract)
-const job1 = new CronJob('30 1-23/2 * * *', retract)
-// const job2 = new CronJob('*/15 * * * *', screenshot)
+const job0 = new CronJob('30 */2 * 12 * ', retract, null, true, 'America/Los_Angeles')
+const job1 = new CronJob('10,50 1-23/2 * 12 *', retract, null, true, 'America/Los_Angeles')
+const job2 = new CronJob('0,40 */2 * 1 * ', retract, null, true, 'America/Los_Angeles')
+const job3 = new CronJob('20 1-23/2 * 1 *', retract, null, true, 'America/Los_Angeles')
+const job4 = new CronJob('*/1 * * 1 *', retract, null, true, 'America/Los_Angeles')
 
-job0.start()
-job1.start()
-// job2.start()
+
+// const job2 = new CronJob('*/15 * * * *', screenshot)
 
 console.log('Timetable Cron Launched')
