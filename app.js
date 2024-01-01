ebd7161808c3c255ec2fc6391946c49fed88cb87
James Kao <jmk17@cornell.edu>
2024-01-01 17:15:43 +0900

    Remove test code

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const job0 = new CronJob('30 */2 * 12 * ', retract, null, true, 'America/Los_Ang
 const job1 = new CronJob('10,50 1-23/2 * 12 *', retract, null, true, 'America/Los_Angeles')
 const job2 = new CronJob('0,40 */2 * 1 * ', retract, null, true, 'America/Los_Angeles')
 const job3 = new CronJob('20 1-23/2 * 1 *', retract, null, true, 'America/Los_Angeles')
-const job4 = new CronJob('*/1 * * 1 *', retract, null, true, 'America/Los_Angeles')
+// const job4 = new CronJob('*/1 * * 1 *', retract, null, true, 'America/Los_Angeles')
 
 
 // const job2 = new CronJob('*/15 * * * *', screenshot)
